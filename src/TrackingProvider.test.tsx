e364a55591bca55b28818330103226e00a3ed470
David Madner <david@tourradar.com>
2020-05-17 23:05:25 +0200

    make tracked events typeable by using the same generic for context and hook

--- src/TrackingProvider.test.tsx
+++ src/TrackingProvider.test.tsx
@@ -1,11 +1,56 @@
 import React from 'react';
 
-import { render, screen } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react-hooks';
 
-import TrackingProvider from './TrackingProvider';
+import {
+  TrackingProvider,
+  useTracking,
+  createTrackingProvider
+} from './TrackingProvider';
 
-test('It should render Hello World', () => {
-  render(<TrackingProvider />);
+test('TrackingProvider - default generic', () => {
+  const trackCallback = jest.fn();
 
-  expect(screen.getByText(/Hello world/i)).toBeInTheDocument();
+  const wrapper = ({ children }) => (
+    <TrackingProvider onEvent={trackCallback}>{children}</TrackingProvider>
+  );
+
+  const { result } = renderHook(() => useTracking(), {
+    wrapper
+  });
+
+  act(() => {
+    result.current.trackEvent({ custom: 'data' });
+  });
+
+  expect(trackCallback).toHaveBeenCalledWith({ custom: 'data' });
+});
+
+test('TrackingProvider with explicit type for logged events', () => {
+  interface TestEvent {
+    custom: number;
+  }
+
+  const {
+    TrackingProvider: CustomTrackingProvider,
+    useTracking: customUseTracking
+  } = createTrackingProvider<TestEvent>();
+
+  const trackCallback = jest.fn();
+
+  const wrapper = ({ children }) => (
+    <CustomTrackingProvider onEvent={trackCallback}>
+      {children}
+    </CustomTrackingProvider>
+  );
+
+  const { result } = renderHook(() => customUseTracking(), {
+    wrapper
+  });
+
+  act(() => {
+    result.current.trackEvent({ custom: 2 });
+  });
+
+  expect(trackCallback).toHaveBeenCalledWith({ custom: 2 });
 });
--- src/TrackingProvider.tsx
+++ src/TrackingProvider.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useContext, FC } from 'react';
 
-const TrackingProvider = () => {
-  return <h1>Hello World</h1>;
+interface Provider<T> {
+  trackEvent: (data: T) => void;
+}
+
+interface Props<T> {
+  onEvent: (data: T) => void;
+}
+
+export const createTrackingProvider = <T,>() => {
+  const Context = React.createContext<Provider<T>>(null);
+
+  const TrackingProvider: FC<Props<T>> = ({ onEvent, children }) => {
+    return (
+      <Context.Provider
+        value={{
+          trackEvent: onEvent
+        }}
+      >
+        {children}
+      </Context.Provider>
+    );
+  };
+
+  const useTracking = () => {
+    const { trackEvent } = useContext(Context);
+
+    return {
+      trackEvent
+    };
+  };
+
+  return {
+    TrackingProvider,
+    useTracking
+  };
 };
 
-export default TrackingProvider;
+// Create context with any generic
+const result = createTrackingProvider<any>();
+
+export const useTracking = result.useTracking;
+export const TrackingProvider = result.TrackingProvider;
