ae28c84e7d5ecdf7728af6bcca82ad396863da1d
David Madner <david.madner@gmail.com>
2020-06-27 11:33:04 +0200

    fix: Don't bundle external dependecies such as React

--- rollup.config.js
+++ rollup.config.js
@@ -7,17 +7,17 @@ import del from 'rollup-plugin-delete';
 import generatePackageJson from 'rollup-plugin-generate-package-json';
 import typescript from 'rollup-plugin-typescript2';
 
-const external = require('./package.json');
-
 const INPUT_FILE_NAME = 'index.ts';
 const INPUT_FOLDER_NAME = 'src';
 
 const OUTPUT_FILE_NAME = 'index.cjs.js';
 const OUTPUT_FOLDER_NAME = 'dist';
 
+const external = ['react'];
+
 export default {
   input: path.join(INPUT_FOLDER_NAME, INPUT_FILE_NAME),
-  external: Object.keys(external.dependencies || {}),
+  external,
   output: {
     file: path.join(OUTPUT_FOLDER_NAME, OUTPUT_FILE_NAME),
     format: 'cjs'
